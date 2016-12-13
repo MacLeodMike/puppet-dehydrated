5669092d5bc3895e784e457e3daa71b035662867
Michael MacLeod <mikemacleod@gmail.com>
2016-12-12 23:14:29 -0500

    Fixups

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -1,11 +1,18 @@
-define dehydrated::certificate {
+define dehydrated::certificate (
+
+  $domains = undef,
+
+) {
+
+  validate_array($domains)
+  include ::dehydrated
 
   concat::fragment { "${dehydrated::etcdir}/domains.txt-${name}":
     target  => "${dehydrated::etcdir}/domains.txt",
     content => inline_template("<%= @name %> <%= @domains.reject { |name| name == @name }.join(' ') %>\n"),
   }
 
-  exec { 'dehydrated':
+  exec { 'dehydrated-${name}':
     command => 'true',
     unless  => "test -r ${dehydrated::etcdir}/certs/${name}/cert.pem",
     path    => '/bin:/usr/bin',
