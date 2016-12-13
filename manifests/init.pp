a0cc0c9181345bb3d6b26558ae970c991f342d8b
Michael MacLeod <mikemacleod@gmail.com>
2016-12-12 23:39:19 -0500

    Adding staging parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,18 +1,20 @@
 class dehydrated (
 
-  $email          = $dehydrated::params::email,
-  $domains        = $dehydrated::params::domains,
-  $user           = $dehydrated::params::user,
-  $group          = $dehydrated::params::group,
-  $cron_enabled   = $dehydrated::params::cron_enabled,
-  $apache_enabled = $dehydrated::params::apache_enabled,
-  $apache_user    = $dehydrated::params::apache_user,
+  $email           = $dehydrated::params::email,
+  $domains         = $dehydrated::params::domains,
+  $user            = $dehydrated::params::user,
+  $group           = $dehydrated::params::group,
+  $staging_enabled = $dehydrated::params::staging_enabled,
+  $cron_enabled    = $dehydrated::params::cron_enabled,
+  $apache_enabled  = $dehydrated::params::apache_enabled,
+  $apache_user     = $dehydrated::params::apache_user,
 
 ) inherits dehydrated::params {
 
   validate_string($email)
   validate_string($user)
   validate_string($group)
+  validate_bool($staging_enabled)
   validate_bool($cron_enabled)
   validate_bool($apache_enabled)
   validate_string($apache_user)
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,12 @@
 class dehydrated::params {
 
-  $email          = undef
-  $domains        = undef
-  $user           = 'dehydrated'
-  $group          = 'dehydrated'
-  $cron_enabled   = true
-  $apache_enabled = false
+  $email           = undef
+  $domains         = undef
+  $user            = 'dehydrated'
+  $group           = 'dehydrated'
+  $staging_enabled = false
+  $cron_enabled    = true
+  $apache_enabled  = false
 
   case $::osfamily {
     'Debian': {
